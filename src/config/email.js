2157e7d7b00cacde68f4e3e8c8f1aeb44ff671fb
BrandoMorales <felipemoralesherrera888@gmail.com>
2026-09-09 16:22:20 -0500

    clao

--- src/config/email.js
+++ src/config/email.js
@@ -5,4 +5,4 @@ export const EMAILJS_TEMPLATE_ID =
   import.meta.env.VITE_EMAILJS_TEMPLATE_ID || "template_c29iebm";
 
 export const EMAILJS_PUBLIC_KEY =
-  import.meta.env.VITE_EMAILJS_PUBLIC_KEY || "6vIfd7D5Dltyqq_MO";
+  import.meta.env.VITE_EMAILJS_PUBLIC_KEY || "WB-xnoNbKku7X_qgL";
